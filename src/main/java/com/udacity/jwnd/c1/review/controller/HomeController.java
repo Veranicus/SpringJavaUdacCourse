d1549a0947dc25bfe370c81a51d1e9b220d94cb2
Patrick <polacek.patrik@gmail.com>
2020-07-05 22:32:09 +0200

    MvcFinal

--- src/main/java/com/udacity/jwnd/c1/review/controller/HomeController.java
+++ src/main/java/com/udacity/jwnd/c1/review/controller/HomeController.java
@@ -1,15 +1,37 @@
 package com.udacity.jwnd.c1.review.controller;
 
+import com.udacity.jwnd.c1.review.service.MessageListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.Instant;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("home")
-    public String getHomePage(){
+    private MessageListService messageListService;
+
+    @Autowired
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        model.addAttribute("welcomeMessage", Instant.now().toString());
+        model.addAttribute("greetings", messageListService.getMessages());
+        return "home";
+    }
+
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        messageListService.addMessage(newMessage.getText());
+        model.addAttribute("greetings", messageListService.getMessages());
+        newMessage.setText("");
         return "home";
     }
 }
